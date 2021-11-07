1f8d4cdce5536d59ab82e4d009cfc9124d97fcfc
FlutLab.io <>
2021-11-07 22:11:03 +0000

    Autosave

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,4 +1,30 @@
-/*SystemChrome.setPreferredOrientations([
+ import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    //Change value to name2
+    SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft,
-  ]);*/
+  ])
+  }
+
+  @override
+  void onClose() {
+    name.value = '';
+    super.onClose();
+     SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  }
+
+
+}
+ 
+ 
+ /*;*/
